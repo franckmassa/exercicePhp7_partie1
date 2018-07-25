a4826dc3240639d1d4a72c6491eeddd6712a2f8a
massa <massafranck@gmail.com>
2018-07-25 21:30:11 +0200

    initial

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$lastname = 'Martin';
+$firstname = 'Isabelle';
+$age = 32;
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8" />
+  <title>Exercice7</title>
+</head>
+<body>
+  <p>
+    <?php
+    echo 'Bonjour '.$lastname.' '.$firstname.', tu as '.$age.' ans.';
+    ?>
+  </p>
+</body>
+</html>
